d743a46c01891a00d7a255c1995c7f507751983c
Tim Dithmer <dith.tim@gmail.com>
2026-08-09 21:52:33 +0200

    feat: make setup_ca.sh create the complete pillar per control_plane and node

--- ca/setup_ca.sh
+++ ca/setup_ca.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
+cd "$(dirname "$(realpath "$0")")" || exit 1
+
+prepare_pillar() {
+  local arg name mode
+
+  if [[ $# -eq 0 ]]; then
+    echo "Usage: prepare_pillar <name1> [<name2>:cert ...]" >&2
+    return 1
+  fi
+
+  {
+    echo "pki:"
+    for arg in "$@"; do
+      # By default, assume we want both, and the name is the full argument
+      mode="both"
+      name="$arg"
+      # If the argument ends with ':cert', change mode and strip the suffix
+      if [[ "$arg" == *":cert" ]]; then
+        mode="cert"
+        name="${arg%:cert}"
+      fi
+      echo "  ${name}:"
+      # Add KEY if in "both" mode AND the file exists
+      if [[ "$mode" == "both" ]] && [[ -f "${name}.key" ]]; then
+        echo "    key: |"
+        sed 's/^/      /' "${name}.key"
+      fi
+      # Add CERT if the file exists
+      if [[ -f "${name}.crt" ]]; then
+        echo "    cert: |"
+        sed 's/^/      /' "${name}.crt"
+      fi
+    done
+  }
+}
 
 # Create CA key and certificate
 {
@@ -19,7 +54,6 @@ certs=(
   "kube-proxy"
   "kube-scheduler"
 )
-
 for i in ${certs[*]}; do
   openssl genrsa -out "${i}.key" 4096
 
@@ -36,7 +70,6 @@ for i in ${certs[*]}; do
 done
 
 account_certs=("service-accounts" "admin")
-
 for i in ${account_certs[*]}; do
   openssl genrsa -out "${i}.key" 4096
 
@@ -50,11 +83,11 @@ for i in ${account_certs[*]}; do
     -CAkey "ca.key" \
     -CAcreateserial \
     -out "${i}.crt"
-  done
+done
 
+prepare_pillar "ca" "kube-api-server" "service-accounts" "kube-controller-manager" "kube-scheduler" "admin" > control_plane_pillar.yaml
 
 node_certs=("node-0" "node-1")
-
 for i in ${node_certs[*]}; do
   # config contains {{ node_name }} placeholder, replace it with the actual node name
   sed "s/{{ node_name }}/${i}/g" node.conf > "node_${i}.conf"
@@ -71,4 +104,8 @@ for i in ${node_certs[*]}; do
     -CAkey "ca.key" \
     -CAcreateserial \
     -out "node_${i}.crt"
+
+  prepare_pillar "ca":cert "$i" | sed 's/'"${i}"'/kubelet/g' > "${i}"_pillar.yaml
 done
+
+rm -rf -- *.csr *.srl *.key *.crt node_*.conf
